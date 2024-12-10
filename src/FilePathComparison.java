9cf7e2a3fe57fb06247260a6cf865d078c2e8dff
SmartW0rker <hristijanchochevski@gmail.com>
2024-12-10 01:00:10 +0100

    First example

---
+++ src/FilePathComparison.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FilePathComparison {
+    public static void main(String[] args) {
+
+        // This is with java.io package and File class
+        System.out.println(new File("").getAbsolutePath());
+
+        String fileName="/files/text.txt";
+
+        File file=new File(new File("").getAbsolutePath(),fileName);
+
+        System.out.println(file.getName());
+        System.out.println("Parent folder for the text.file is: "+ file.getParentFile().getName());
+        if (!file.exists()) {
+            System.out.println("1.I can't run unless this file exists");
+            return;
+        }
+        System.out.println("1.I'm good to go.");
+
+        for (File root: File.listRoots()){
+            System.out.println(root);
+        }
+
+        Path path= Paths.get(fileName);
+        if (Files.exists(path)) {
+            System.out.println("2.I can't run unless this file exists");
+            return;
+        }
+        System.out.println("2.I'm good to go.");
+    }
+}
